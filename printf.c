d62a1aa3ad227125f43e8060e530f57eb20a5471
Matteo-lu <mateolondono.u@gmail.com>
2021-03-16 16:25:16 +0000

    Upload with betty style for the first time

--- printf.c
+++ printf.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * int _printf - prints any kind of element
+ * _printf - prints any kind of element
  * @format: reads the string given by the user
  * Return: return the number of prints
  */
@@ -21,22 +21,22 @@ int _printf(const char *format, ...)
 	};
 
 	va_list aux;
-	va_start(aux, format);
 
-	for (i = 0; format [i] != '\0'; i++)
+	va_start(aux, format);
+	for (i = 0; format[i] != '\0'; i++)
 	{
-		if (format [i] == '%' && format[i + 1] == 's')
+		if (format[i] == '%' && format[i + 1] == 's')
 		{
-			for (j=0; imp[j].s != '\0'; j++)
+			for (j = 0; imp[j].s != '\0'; j++)
 			{
-				if (imp [j].s[0] ==  format [i + 1])
+				if (imp[j].s[0] ==  format[i + 1])
 				{
 					imp[j].f(aux);
-	  			}
+				}
 			}
 		i = i + 2;
 		}
-		
+
 		if (format[i] == '%')
 		i = i - 1;
 
@@ -45,40 +45,4 @@ int _printf(const char *format, ...)
 			write(1, &n, 1);
 	}
 	va_end(aux);
-}
-
-void linea(va_list aux)
-{
-  int i,n;
-  char *z;
-  
-  z = va_arg(aux, char*);
-
-  for (i = 0;z[i] != '\0'; i++)
-	{
-	  n = z[i];
-	  write(1, &n, 1);
-	}
-}
-
-void caracter(va_list aux)
-{
-  int z;
-  
-  z = va_arg(aux, int);
-  write(1, &z, 1);
-}
-
-void entero(va_list aux)
-{
-  int z;
-
-  z = va_arg(aux, int);
-  printf("%d", z);
-}
-
-int main(void)
-{
-	_printf("Let's try to printf a %s%s simple sentence. %c\n", "fucking","fucking", 'T');
-	return (0);
 }
